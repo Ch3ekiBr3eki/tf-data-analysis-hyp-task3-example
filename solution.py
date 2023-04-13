74c3993f2a9769f80655b1d6c0d818372165b71a
Anton <113473409+Ch3ekiBr3eki@users.noreply.github.com>
2023-04-13 20:40:17 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -3,17 +3,23 @@ import numpy as np
 from scipy.stats import ttest_ind
 import scipy.stats as st
 
-chat_id = 323297403  # Ваш chat ID, не меняйте название переменной
+chat_id = 323297403  # Your chat ID, do not change the variable name
 
 def solution(control: np.ndarray, test: np.ndarray) -> bool:
-    alpha = 0.04 
+    alpha = 0.04
 
     control_mean = np.mean(control)
     test_mean = np.mean(test)
+    control_std = np.std(control, ddof=1)
+    test_std = np.std(test, ddof=1)
 
+    dof = len(control) + len(test) - 2
+    pooled_std = np.sqrt(((len(control)-1) * control_std ** 2 + (len(test)-1) * test_std ** 2) / dof)
+    effect_size = abs(test_mean - control_mean) / pooled_std
+    
     t_statistic, p_value = st.ttest_ind(control, test, equal_var=False)
 
-    if p_value < alpha:
+    if p_value < alpha and effect_size > 0.2:
         return True
     else:
         return False
