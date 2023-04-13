3585676a060b90ccd69d8feda851b868a006e99f
Anton <113473409+Ch3ekiBr3eki@users.noreply.github.com>
2023-04-13 20:18:36 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,10 +1,19 @@
 import pandas as pd
 import numpy as np
 from scipy.stats import ttest_ind
+import scipy.stats as st
 
 chat_id = 323297403  # Ваш chat ID, не меняйте название переменной
 
 def solution(control: np.ndarray, test: np.ndarray) -> bool:
-    _, p_value = ttest_ind(control, test, equal_var=False)
-    significance_level = 0.04
-    return p_value < significance_level
+    alpha = 0.04 
+
+    control_mean = np.mean(control)
+    test_mean = np.mean(test)
+
+    t_statistic, p_value = st.ttest_ind(control, test, equal_var=False)
+
+    if p_value < alpha:
+        return True
+    else:
+        return False
